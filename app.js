ff15902ec69380993f3157838aa4869110090fd1
Catfish7887 <danya.kleutin@yandex.ru>
2022-12-19 22:24:04 +0300

    some fixes

--- app.js
+++ app.js
@@ -2,12 +2,19 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const { errors } = require('celebrate');
-const { constants } = require('http2');
+// const { errors } = require('celebrate');
+// const { constants } = require('http2');
 const helmet = require('helmet');
 const cors = require('cors');
+const { appRouter } = require('./routes');
 
 const app = express();
 mongoose.connect(process.env.DB_URL);
+
+app.use(helmet());
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use(appRouter);
 
 app.listen(process.env.PORT);
--- models/movie.js
+++ models/movie.js
@@ -60,6 +60,7 @@ const movieSchema = new mongoose.Schema({
 
   },
   movieId: {
+    type: String,
     required: true,
 
   },
