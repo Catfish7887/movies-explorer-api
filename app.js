3b0f84794aabb7e7285c0bc90aea8a99635f02b9
Catfish7887 <danya.kleutin@yandex.ru>
2022-12-22 16:31:39 +0300

    Некоторые изменения

--- app.js
+++ app.js
@@ -7,14 +7,20 @@ const bodyParser = require('body-parser');
 const helmet = require('helmet');
 const cors = require('cors');
 const { appRouter } = require('./routes');
+const { errorsHandler } = require('./middlewares/errorsHandler');
 
 const app = express();
 mongoose.connect(process.env.DB_URL);
+mongoose.set('strictQuery', true);
 
 app.use(helmet());
 app.use(cors());
 app.use(bodyParser.json());
 
+// Роутинг. Чтобы не грузить app.js, вынес роуты в отдельные файлы
 app.use(appRouter);
+
+// Централизованный обработчик ошибок
+app.use(errorsHandler);
 
 app.listen(process.env.PORT);
