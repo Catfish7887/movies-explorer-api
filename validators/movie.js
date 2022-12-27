1f7f93076a9f0f20fcceeaedff5fece7646f5a5e
Catfish7887 <danya.kleutin@yandex.ru>
2022-12-28 02:32:08 +0300

    #9 Добавлена валидация с помощью joi

---
+++ validators/movie.js
@@ -0,0 +1,33 @@
+const { Joi, Segments } = require('celebrate');
+
+const { celebrate, schemaObjectId, schemaURL } = require('./utils');
+
+const joiMovieSchema = Joi.object({
+  country: Joi.string().required(),
+  director: Joi.string().required(),
+  duration: Joi.number().required(),
+  year: Joi.string().required(),
+  description: Joi.string().required(),
+  image: schemaURL.required(),
+  trailerLink: schemaURL.required(),
+  thumbnail: schemaURL.required(),
+  movieId: Joi.string().required(),
+  nameRU: Joi.string().required(),
+  nameEN: Joi.string().required(),
+
+});
+
+const joiSchemaId = Joi.object({
+  Id: schemaObjectId.required(),
+});
+
+const movieSegment = { [Segments.BODY]: joiMovieSchema };
+const idSegment = { [Segments.PARAMS]: joiSchemaId };
+
+const validateMovie = celebrate(movieSegment);
+const validateId = celebrate(idSegment);
+
+module.exports = {
+  validateMovie,
+  validateId,
+};
